f955aab629521fda6a05076e87b06b3e1a5cd3c1
Alvin-Liu <alvin.llm@foxmail.com>
2020-10-19 06:35:47 +0800

    feat: 添加百度统计

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -36,6 +36,22 @@ module.exports = {
       ]
     }
   },
+  head: [
+    // 添加百度统计
+    [
+      "script",
+      {},
+      `
+      var _hmt = _hmt || [];
+      (function() {
+        var hm = document.createElement("script");
+        hm.src = "https://hm.baidu.com/hm.js?${process.env.BAIDU_ANALYTICS}";
+        var s = document.getElementsByTagName("script")[0];
+        s.parentNode.insertBefore(hm, s);
+      })();
+        `
+    ]
+  ],
   plugins: [
     [
       '@vuepress/google-analytics',
